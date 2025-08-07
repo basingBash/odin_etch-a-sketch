13fc86bf8361fb27c701a4ab599bbc0b465b56cb
basingBash <davidjuancarlos1127@gmail.com>
2025-08-07 16:13:23 +0800

    .

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const containerElement = document.querySelector('.container');
+    const btn = document.querySelector('.btn');
+    const clear = document.querySelector('.clr');
+
+    btn.addEventListener('click', () => {
+        let gridsize;
+        do {
+            gridsize = prompt('Grid size:', );
+        } while(!gridsize || gridsize > 100 || isNaN(gridsize) || gridsize <= 0)
+        generateGrid(gridsize);
+        
+    })
+    clear.addEventListener('click',  () =>  {
+        containerElement.innerHTML = '';
+    })
+    
+    function generateGrid(gridsize) {
+        const totalGrid = gridsize * gridsize;
+        for(let i = 1; i <= totalGrid; i++) {
+            const div = document.createElement('div');
+            div.setAttribute('style', `border: 0.5px solid black; background-color: gray; width: calc(100% /${gridsize}); height: calc(100% / ${gridsize})`)
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = 'white';
+            })
+            containerElement.appendChild(div);
+        }
+    }
+
+})
